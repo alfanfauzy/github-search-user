ef0c16932833bddb620e8c699b3819cb489aae34
alfanfauzy <zhafhy@gmail.com>
2025-12-29 12:06:58 +0700

    fix(Style): update view user list

--- src/App.tsx
+++ src/App.tsx
@@ -6,63 +6,60 @@ import { useGetUsersRepoListViewModal } from "./core/view/usersRepo/view-models/
 import ListUserMolecules from "./core/view/users/components/molecules/listUser";
 
 function App() {
-    const [inputValue, setInputValue] = useState("");
-    const [searchValue, setSearchValue] = useState("");
-    const [selectedUser, setSelectedUser] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedUser, setSelectedUser] = useState("");
 
-    const { data: ListUser, isLoading } = useGetUsersListViewModal(
-        searchValue,
-        {
-            enabled: searchValue !== "",
-        }
-    );
+  const { data: ListUser, isLoading } = useGetUsersListViewModal(searchValue, {
+    enabled: searchValue !== "",
+  });
 
-    const {
-        data: ListRepo,
-        refetch,
-        isLoading: isLoadingListRepo,
-    } = useGetUsersRepoListViewModal(selectedUser, {
-        enabled: selectedUser.length !== 0,
-    });
+  const {
+    data: ListRepo,
+    refetch,
+    isLoading: isLoadingListRepo,
+  } = useGetUsersRepoListViewModal(selectedUser, {
+    enabled: selectedUser.length !== 0,
+  });
 
-    const handleSelectedUser = (key: string | string[]) => {
-        setSelectedUser(key as string);
+  const handleSelectedUser = (key: string | string[]) => {
+    setSelectedUser(key as string);
 
-        if (selectedUser.length !== 0) {
-            refetch();
-        }
-    };
+    if (selectedUser.length !== 0) {
+      refetch();
+    }
+  };
 
-    const isEmptyInput = inputValue === "";
-    const isEmptySearch = searchValue === "";
+  const isEmptyInput = inputValue === "";
+  const isEmptySearch = searchValue === "";
 
-    return (
-        <div className="bg-slate-200 h-screen w-screen text-black">
-            <div className="pt-10 m-auto w-[500px] border border-neutral-700mx-auto min-h-screen shadow-md bg-white px-5">
-                <h2 className="font-bold text-xl py-3 text-center">
-                    Search Github User
-                </h2>
+  return (
+    <div className="bg-slate-200 h-screen w-screen text-black py-4 px-4">
+      <div className="pt-10 m-auto h-[calc(100vh-2rem)] w-[500px] border mx-auto shadow-md bg-white px-5 rounded-xl flex flex-col">
+        <h2 className="font-bold text-xl py-3 text-center">
+          Search Github User
+        </h2>
 
-                <SearchBox
-                    searchValue={searchValue}
-                    inputValue={inputValue}
-                    setInputValue={setInputValue}
-                    setSearchValue={setSearchValue}
-                />
+        <SearchBox
+          searchValue={searchValue}
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          setSearchValue={setSearchValue}
+        />
 
-                {isLoading && <AtomsLoading />}
+        {isLoading && <AtomsLoading />}
 
-                {!isEmptyInput && !isEmptySearch && ListUser && (
-                    <ListUserMolecules
-                        handleSelectPanel={handleSelectedUser}
-                        dataUser={ListUser}
-                        dataRepo={ListRepo}
-                        isLoadingRepo={isLoadingListRepo}
-                    />
-                )}
-            </div>
-        </div>
-    );
+        {!isEmptyInput && !isEmptySearch && ListUser && (
+          <ListUserMolecules
+            handleSelectPanel={handleSelectedUser}
+            dataUser={ListUser}
+            dataRepo={ListRepo}
+            isLoadingRepo={isLoadingListRepo}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/core/view/users/components/molecules/listUser/index.tsx
+++ src/core/view/users/components/molecules/listUser/index.tsx
@@ -6,54 +6,48 @@ import { UserLists } from "src/core/domain/users/models";
 import { UserRepoLists } from "src/core/domain/usersRepo/models";
 
 type ListUserMoleculesProps = {
-    handleSelectPanel: (key: string | string[]) => void;
-    dataUser: UserLists;
-    dataRepo: UserRepoLists | undefined;
-    isLoadingRepo: boolean;
+  handleSelectPanel: (key: string | string[]) => void;
+  dataUser: UserLists;
+  dataRepo: UserRepoLists | undefined;
+  isLoadingRepo: boolean;
 };
 
 const { Panel } = Collapse;
 
 const ListUserMolecules = ({
-    handleSelectPanel,
-    dataUser,
-    dataRepo,
-    isLoadingRepo,
+  handleSelectPanel,
+  dataUser,
+  dataRepo,
+  isLoadingRepo,
 }: ListUserMoleculesProps) => {
-    return (
-        <div>
-            <div className="w-full h-[500px] overflow-auto border-2 rounded-lg ">
-                <Collapse onChange={handleSelectPanel} accordion>
-                    {dataUser.map((user) => (
-                        <Panel
-                            header={
-                                <h1 className="text-base font-bold">
-                                    {user.name}
-                                </h1>
-                            }
-                            key={user.name}
-                        >
-                            {isLoadingRepo && <AtomsLoading />}
+  return (
+    <div className="flex-1 min-h-0 py-2 flex flex-col">
+      <div className="w-full flex-1 overflow-y-auto border-2 rounded-lg">
+        <Collapse onChange={handleSelectPanel} accordion>
+          {dataUser.map((user) => (
+            <Panel
+              header={<h1 className="text-base font-bold">{user.name}</h1>}
+              key={user.name}
+            >
+              {isLoadingRepo && <AtomsLoading />}
 
-                            {dataRepo && (
-                                <div className="flex flex-col gap-3">
-                                    {dataRepo.map((repo) => (
-                                        <ListRepoMolecules
-                                            dataRepositories={repo}
-                                        />
-                                    ))}
-                                </div>
-                            )}
-                        </Panel>
-                    ))}
-                </Collapse>
-            </div>
-            <p className="text-sm mt-2 text-left">
-                Total found :{" "}
-                <span className="font-bold">{dataUser.length} data</span>
-            </p>
-        </div>
-    );
+              {dataRepo && (
+                <div className="flex flex-col gap-3">
+                  {dataRepo.map((repo) => (
+                    <ListRepoMolecules key={repo.id} dataRepositories={repo} />
+                  ))}
+                </div>
+              )}
+            </Panel>
+          ))}
+        </Collapse>
+      </div>
+
+      <p className="text-sm mt-2 text-left shrink-0">
+        Total found : <span className="font-bold">{dataUser.length} data</span>
+      </p>
+    </div>
+  );
 };
 
 export default ListUserMolecules;
